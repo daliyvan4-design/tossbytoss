abe2132530de98c329180ed1e771a82de3e0ba1e
Toss by Toss <bobdali127@gmail.com>
2026-07-03 08:37:55 +0000

    Revert "revert: retirer la section 360° du logo T (page, composant, CSS, env vidéos)"
    This reverts commit 8f7f38d3921d3af564f7e0a435a206f0a66dbf2f.

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import type { Metadata } from "next";
+import LogoReel from "@/components/LogoReel";
 import PopupBanner from "@/components/PopupBanner";
 
 export const metadata: Metadata = {
@@ -81,6 +82,7 @@ export default function HomePage() {
         </div>
       </section>
 
+      <LogoReel />
       <PopupBanner />
     </>
   );
